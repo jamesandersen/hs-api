528cb6d6a80d994bf407d6f6c95cf86f60379b4d
James Andersen <james.andersen@universalmind.com>
2015-02-21 23:47:25 -0700

    Updated to use https

--- server.js
+++ server.js
@@ -3,15 +3,73 @@
  */
 
 var jsonServer = require('json-server');
+var https = require('https');
+var fs = require('fs');
+
+
 
 var object = {
-    posts: [
-        { id: 1, body: 'foo' }
+    profiles: [
+        {
+            id: 117787249652700998593,
+            userId: '117787249652700998593',
+            students: [
+                {
+                    id: "2342342",
+                    name: "Ella",
+                    calendarIDs: ["jander.me_vhfj574k3cb837tgqe6vrv6e0g@group.calendar.google.com"]
+                },
+                {
+                    id: "aewaef",
+                    name: "Nina",
+                    calendarIDs: ["jander.me_dmq4di5r67vgkn78u6etvsqa1c@group.calendar.google.com"]
+                },
+                {
+                    id: "erweafe2",
+                    name: "Phoebe",
+                    calendarIDs: ["jander.me_1e0uquv8m1lh260s4b0i7h2s7o@group.calendar.google.com"]
+                }
+            ],
+            subjects: [
+                {
+                    id: 'abc',
+                    summary: 'Spanish',
+                    bgcolor: '#33691e'
+                },
+                {
+                    id: 'def',
+                    summary: 'Writing',
+                    bgcolor: '#ff5722'
+                },
+                {
+                    id: 'ghi',
+                    summary: 'History',
+                    bgcolor: '#004d40'
+                },
+                {
+                    id: 'jkl',
+                    summary: 'Math',
+                    bgcolor: '#0091ea'
+                }
+            ]
+        }
     ]
 };
 
 var router = jsonServer.router(object); // Express router
 var server = jsonServer.create();       // Express server
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 9002;
+
+// This line is from the Node.js HTTPS documentation.
+var options = {
+    key: fs.readFileSync('cert/server.key'),
+    cert: fs.readFileSync('cert/server.crt')
+};
+
 server.use(router);
-server.listen(port);
+if(process.env.hasOwnProperty('AZURE')){
+    //server.listen(port);
+} else {
+    // Create an HTTPS service identical to the HTTP service.
+    https.createServer(options, server).listen(port);
+}
