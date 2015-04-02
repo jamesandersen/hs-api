53a041528c16ab25c70ee57ea2d094392757c84e
James Andersen <james.andersen@universalmind.com>
2015-04-01 21:32:08 -0600

    Allow https key/cert to be passed in as arguments

--- server.js
+++ server.js
@@ -6,72 +6,33 @@ var jsonServer = require('json-server');
 var https = require('https');
 var fs = require('fs');
 
-
-
-var object = {
-    profiles: [
-        {
-            id: 117787249652700998593,
-            userId: '117787249652700998593',
-            students: [
-                {
-                    id: "2342342",
-                    name: "Ella",
-                    calendarIDs: ["jander.me_vhfj574k3cb837tgqe6vrv6e0g@group.calendar.google.com"]
-                },
-                {
-                    id: "aewaef",
-                    name: "Nina",
-                    calendarIDs: ["jander.me_dmq4di5r67vgkn78u6etvsqa1c@group.calendar.google.com"]
-                },
-                {
-                    id: "erweafe2",
-                    name: "Phoebe",
-                    calendarIDs: ["jander.me_1e0uquv8m1lh260s4b0i7h2s7o@group.calendar.google.com"]
-                }
-            ],
-            subjects: [
-                {
-                    id: 'abc',
-                    summary: 'Spanish',
-                    bgcolor: '#33691e'
-                },
-                {
-                    id: 'def',
-                    summary: 'Writing',
-                    bgcolor: '#ff5722'
-                },
-                {
-                    id: 'ghi',
-                    summary: 'History',
-                    bgcolor: '#004d40'
-                },
-                {
-                    id: 'jkl',
-                    summary: 'Math',
-                    bgcolor: '#0091ea'
-                }
-            ]
-        }
-    ]
-};
-
-var router = jsonServer.router(object); // Express router
+var router = jsonServer.router(__dirname + '/db.json'); // Express router
 var server = jsonServer.create();       // Express server
 var port = process.env.PORT || 9002;
-
-
 
 server.use(router);
 if(port !== 9002){
     server.listen(port);
 } else {
 
-    // This line is from the Node.js HTTPS documentation.
-    var options = {
-        key: fs.readFileSync('cert/server.key'),
-        cert: fs.readFileSync('cert/server.crt')
-    };
+    // Parse options
+    var optionsRegex = /^-(.*)=(.*)$/,
+        match = null,
+        options = {};
+    process.argv.forEach(function (val, index, array) {
+        if(match = val.match(optionsRegex)) {
+            options[match[1]] = match[2];
+            // console.log(match[1]+ ': ' + match[2]);
+
+            if(match[1] == "key" || match[1] == "cert") {
+                options[match[1]] = fs.readFileSync(match[2]);
+            }
+        }
+    });
+
+    // Fallback to local cert if cert options not passed in
+    options.key = options.key || fs.readFileSync(__dirname + '/cert/server.key');
+    options.cert = options.cert || fs.readFileSync(__dirname + '/cert/server.crt');
 
     // Create an HTTPS service identical to the HTTP service.
     https.createServer(options, server).listen(port);
